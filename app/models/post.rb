28040f4a9c7e2bc8d5a08e61b5c4d61b5bfa291e
prius2055 <prius2055@gmail.com>
2023-07-13 15:52:18 +0100

    update post model. Add methods for five most recent comments from a user.Update posts counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
+
+  def update_user_posts_counter
+        user.update(posts_counter:(posts.count))
+    end
+
+    def five_most_recent_comments
+        comments.order(created_at :desc).limit(5)
+    end
 end
