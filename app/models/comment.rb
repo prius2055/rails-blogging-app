ea04e10db83d063f3013e5486057f21e7949767f
prius2055 <prius2055@gmail.com>
2023-07-13 16:13:33 +0100

    Update like.rb, method to update likes counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,5 +4,7 @@ class Comment < ApplicationRecord
 
   def update_post_comments_counter
     post.update(comments_counter: comments.count)
+    five_most_recent_comments_from_a_user = comments.find_by(author_id: id)
+    five_most_recent_comments_from_a_user.order(created_at: :desc).limit(3)
   end
 end
