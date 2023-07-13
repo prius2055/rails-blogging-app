895bc843ffbcbc9f71a1c8a7dfa1b42bcbdc6978
prius2055 <prius2055@gmail.com>
2023-07-13 16:02:03 +0100

    fix linters error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   def update_post_comments_counter
-        post.update(comments_counter: comments.count)
-    end
+    post.update(comments_counter: comments.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,10 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: :author_id
 
   def update_user_posts_counter
-        user.update(posts_counter:(posts.count))
-    end
+    user.update(posts_counter: posts.count)
+  end
 
-    def five_most_recent_comments
-        comments.order(created_at :desc).limit(5)
-    end
+  def five_most_recent_comments
+    comments.order(created_at(:desc)).limit(5)
+  end
 end
