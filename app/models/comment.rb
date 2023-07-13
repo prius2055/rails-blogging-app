bed4ed53478ecfbd8f5380581535d45405238848
prius2055 <prius2055@gmail.com>
2023-07-13 15:47:37 +0100

    update commit model. Add methods for three most recent post from a user

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def three_most_recent_post
+    three_most_recent_post_from_a_user = posts.find_by(author_id:id)
+    three_most_recent_post_from_a_user.order(created_at: :desc).limit(3)
+  end
 end
