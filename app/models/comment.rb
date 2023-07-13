b7f525da2c32300f33659577dbcd0702d67df7ae
prius2055 <prius2055@gmail.com>
2023-07-13 15:54:33 +0100

    update comment model.Add methods to update comments counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def update_post_comments_counter
+        post.update(comments_counter: comments.count)
+    end
 end
