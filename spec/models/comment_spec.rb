22182a854336e231872fa7f167a25889b83aeaf8
prius2055 <prius2055@gmail.com>
2023-07-14 19:06:51 +0100

    Fix Linters error

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,15 +1,16 @@
-
 require 'rails_helper'
 
 describe Like, type: :model do
   describe ' #update_post_comments_counter' do
     let(:first_user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0) }
-    let(:first_post) {Post.create(author: first_user, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)}
-    
-    let(:first_comment) { Comment.create(author: first_user, post:first_post) }
+    let(:first_post) do
+      Post.create(author: first_user, title: 'Hello', text: 'This is my first post', comments_counter: 0,
+                  likes_counter: 0)
+    end
+
+    let(:first_comment) { Comment.create(author: first_user, post: first_post) }
 
     it 'increments the comments_counter of the associated post' do
-    
       expect { first_comment.update_post_comments_counter }.to change { first_post.reload.comments_counter }.by(1)
     end
   end
