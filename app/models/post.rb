18303d040fbaad6ad65c0bd66bba9c2556116232
prius2055 <prius2055@gmail.com>
2023-07-14 11:33:30 +0100

    Add validation for post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence:true length: { maximum: 250 }
+  validates :comments_counter, comparison:{greater_than_or_equal_to: 0} 
+  validates :likes_counter, comparison:{greater_than_or_equal_to: 0} 
+
   def update_user_posts_counter
     author.increment!(:posts_counter)
   end
