4c842d35184c52d9ade79009d5b25223b60920d3
prius2055 <prius2055@gmail.com>
2023-07-14 19:00:41 +0100

    Add test to Comment model.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,1 +1,17 @@
 
+require 'rails_helper'
+
+describe Like, type: :model do
+  describe ' #update_post_comments_counter' do
+    let(:first_user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0) }
+    let(:first_post) 
+      {Post.create(author: first_user, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)}
+    
+    let(:first_comment) { Comment.create(author: first_user, post:first_post) }
+
+    it 'increments the comments_counter of the associated post' do
+    
+      expect { first_comment.update_post_likes_counter }.to change { first_post.reload.likes_counter }.by(1)
+    end
+  end
+end
