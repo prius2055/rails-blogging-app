875251f0bd9c47f2c3ef031a536462cb6d293d7d
prius2055 <prius2055@gmail.com>
2023-07-16 16:34:54 +0100

    Add users and posts resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  resources :users, only:[:index, :show] do
+    resources :posts, only:[:index, :show]
+  end
 
+  # get '/users', to: 'users#index'
+  # get '/users/:id', to: 'users#show'
+
+  # get '/users/:id/posts', to: 'posts#show'
+  # get '/users/:id/posts/:id', to: 'posts#show'
 
 end
