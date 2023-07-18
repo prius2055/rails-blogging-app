1465794e1c8df32732f2b2ab5f01acc57ce596bb
prius2055 <prius2055@gmail.com>
2023-07-18 17:34:36 +0100

    Add test for show action

--- spec/request/user_spec.rb
+++ spec/request/user_spec.rb
@@ -9,5 +9,25 @@ describe UsersController, type: :request do
       get users_path
       expect(response).to render_template(:index)
     end
+
+    it 'includes correct placeholder text in the response body' do
+      get users_path
+      expect(response.body).to include('Here is a list of all users')
+    end
+  end
+
+  describe '#show' do
+
+      let(:user) { User.create(name: 'Benny') }
+
+    it 'returns a successful response' do
+      get users_path(user)
+      expect(response).to be_successful
+    end
+
+    it 'renders the show template' do
+      get users_path(user.id)
+      expect(response).to render_template(:show)
+    end
   end
 end
