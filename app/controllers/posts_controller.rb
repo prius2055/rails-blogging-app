44491d4be49d76b042004498c7e67776ca2fca03
prius2055 <prius2055@gmail.com>
2023-07-21 17:07:59 +0100

    Update the post view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,26 @@ class PostsController < ApplicationController
   end
 
    def new
-    @post = Post.new(@active_user)
+    @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+    @post.likes_counter = 0
+    @post.comments_counter = 0
+
+   if @post.save
+      redirect_to users_path, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+    private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
