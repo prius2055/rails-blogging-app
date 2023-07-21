960f91868ff8c658528ecbb8df948654dedec78c
prius2055 <prius2055@gmail.com>
2023-07-21 17:15:17 +0100

    Create CommentsController

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only:[:index, :show] do
     resources :posts, only:[:index, :new, :show, :create]
+    resources  :comments, only:[:new, :create]
   end
 
 end
