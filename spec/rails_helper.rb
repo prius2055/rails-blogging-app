bf1a4d074f692c62046d85d98bcc12fd9a6872de
prius2055 <prius2055@gmail.com>
2023-08-01 23:34:29 +0100

    Fix linters error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,10 +20,6 @@ Webdrivers::Chromedriver.required_version = '114.0.5735.90'
 
 # Capybara.javascript_driver = :selenium_chrome
 # Capybara.server = :puma
-
-
-
-
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -49,7 +45,6 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
