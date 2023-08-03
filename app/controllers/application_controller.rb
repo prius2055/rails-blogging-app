9fe330ed641cece61e35265e6f19af8c0fe82611
prius2055 <prius2055@gmail.com>
2023-08-03 17:40:28 +0100

    update db to allow authentication and confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!
+
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
@@ -9,4 +11,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :posts_counter) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
+
 end
